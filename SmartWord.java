784f9bf079b20a94e695124ff37692c5bce32fed
ryanbomo <ryanbomo@gmail.com>
2016-11-28 14:34:16 -0500

    Added description of algorithm - Ryan

--- SmartWord.java
+++ SmartWord.java
@@ -8,35 +8,53 @@
   Section: 4
 
   Description of the overall algorithm:
-      PSEUDOCODE WILL GO HERE
+1. Grab word list and build trie from the word list [Done in SmartWord]
+    a. Initial weight for each word is 0 to allow for each word to be a valid guess
+        But without having to worry about whether it's been used or not
+2. Process the old messages by chopping the file into lines, and then the lines
+    into words. [Done in processOldMessages]
+        If the word is in the trie, increment its weight by one and restructure trie
+        Else put the word in the trie with weight of 1 
+3. Guess by traversing the trie and returning highest weighted words that could
+    possibly start with letters typed so far
+4. Evaluate our accuracy
+        If accurate, increment weight of word by 1
+        If Inaccurate but target is in trie, increment weight of correct word by 1
+        If Inaccurate but target is not in trie, add to trie and set weight to 1
 
 
-*/
+ */
+package SmartWord;
 
+public class SmartWord {
 
-public class SmartWord
-{
     String[] guesses = new String[3];  // 3 guesses from SmartWord
 
     // initialize SmartWord with a file of English words
-    public SmartWord(String wordFile)
-    {
+    public SmartWord(String wordFile) {
+        /*
+        Initialize Trie using word list argument
+        Initial weight for everything in trie should be 1
+            This allows things to be a valid guess from the beginning
+        Once the Trie is built, the processOldMessages will bring in the new
+        words to modify the weight.
+         */
 
     }
 
     // process old messages from oldMessageFile
-    public void processOldMessages(String oldMessageFile)
-    {
-     
+    public void processOldMessages(String oldMessageFile) {
+        String lineBreak = System.getProperty("line.separator");
+        String[] individualLines = oldMessageFile.split(lineBreak);
+
     }
 
     // based on a letter typed in by the user, return 3 word guesses in an array
     // letter: letter typed in by the user
     // letterPosition:  position of the letter in the word, starts from 0
     // wordPosition: position of the word in a message, starts from 0
-    public String[] guess(char letter,  int letterPosition, int wordPosition)
-    {
-	
+    public String[] guess(char letter, int letterPosition, int wordPosition) {
+
         return guesses;
     }
 
@@ -53,8 +71,7 @@ public class SmartWord
     // a.         true                correct word
     // b.         false               null
     // c.         false               correct word
-    public void feedback(boolean isCorrectGuess, String correctWord)        
-    {
+    public void feedback(boolean isCorrectGuess, String correctWord) {
 
     }
 
